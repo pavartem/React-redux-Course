cb2afa76a9b1217b098e262c7ebd77c63e58ba45
zennarchi <art256100@gmail.com>
2019-03-04 21:00:20 +0200

    Worked with controlled third-party components and default form inputs.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
+import Select from 'react-select'
 import ArticleList from './components/article-list'
 import ArticleChart from './components/article-chart'
 import articles from './fixtures'
+import UserForm from './components/user-form'
 
 class App extends Component {
+  state = {
+    openItem: null
+  }
+
   render() {
     return (
       <div>
-        <ArticleList articles={articles}/>
+        <UserForm/>
+        <Select options={this.options} value={this.state.openItem} onChange={this.handleSelect}/>
+        <ArticleList articles={articles} ref={this.setArticleListRef}/>
         <ArticleChart articles={articles}/>
       </div>
     )
+  }
+
+  get options() {
+    return articles.map(article => ({
+      label: article.title,
+      value: article.id
+    }))
+  }
+
+  handleSelect = openItem => this.setState({ openItem })
+
+  setArticlesListRef = ref => {
+    console.log('---', ref, findDOMNode(ref))
+    // setTimeout(() => {
+    //   ref.setState({ openItemId: articles[0].id }) // Не можна так робити
+    //
+    // }, 1000)
   }
 }
 
